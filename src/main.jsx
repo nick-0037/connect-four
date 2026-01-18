a1e35a0c865ada5b424dbac53512b67fb882b249
nick-0073 <120076510+nick-0037@users.noreply.github.com>
2026-01-18 16:50:56 -0400

    Refactor routing in main component; add Navigate for default home redirection

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import App from './App.jsx'
-import GameRoom from './components/GameRoom.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import App from "./App.jsx";
+import GameRoom from "./components/GameRoom.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
       <Routes>
-        <Route path='/home' element={<App/>}/>
-        <Route path='/game/:roomCode' element={<GameRoom/>}/>
+        <Route path="/" element={<Navigate to="/home" replace />} />
+        <Route path="/home" element={<App />} />
+        <Route path="/game/:roomCode" element={<GameRoom />} />
       </Routes>
     </Router>
   </StrictMode>,
-)
+);
